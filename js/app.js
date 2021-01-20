02a3aacab0b9c7ed3dc08b6ed24029fc3695c90a
Collin Zeets <ckzeets@gmail.com>
2021-01-19 20:31:44 -0600

    added chart data

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 "use strict";
 let imageElements = document.getElementsByTagName("img");
-let indexes = undefined;
-const rounds = 25;
+let indexes = [-1, -1, -1];
+let roundCount = 0;
+const rounds = 5;
 class Product {
   constructor(name, path) {
     this.name = name;
@@ -26,7 +27,7 @@ const products = [
   new Product("scissors", "images/scissors.jpg"),
   new Product("shark", "images/shark.jpg"),
   new Product("sweep", "images/sweep.png"),
-  new Product("tauntaun", "images/tantaun.jpg"),
+  new Product("tauntaun", "images/tauntaun.jpg"),
   new Product("unicorn", "images/unicorn.jpg"),
   new Product("usb", "images/usb.gif"),
   new Product("water-can", "images/water-can.jpg"),
@@ -36,19 +37,14 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 function getRandomProducts() {
-  let ret = [getRandomInt(products.length)];
-  while (true) {
-    let num = getRandomInt(products.length);
-    if (ret[0] !== num) {
-      ret.push(num);
-      break;
-    }
-  }
-  while (true) {
-    let num = getRandomInt(products.length);
-    if (ret[0] !== num && ret[1] !== num) {
-      ret.push(num);
-      break;
+  let ret = [];
+  for (let i = 0; i < 3; i++) {
+    while (true) {
+      let num = getRandomInt(products.length);
+      if (!indexes.includes(num) && !ret.includes(num)) {
+        ret.push(num);
+        break;
+      }
     }
   }
   console.log(ret);
@@ -61,28 +57,97 @@ function doRound() {
     products[indexes[i]].shownCount++;
   }
 }
-doRound();
-for (let i = 0; i < imageElements.length; i++) {
-  console.log("Event listed on the images");
-  imageElements[i].addEventListener("click", ifImageClicked);
-}
-let roundCount = 0;
-function ifImageClicked(event) {
-  roundCount += 1;
-  console.log(event.srcElement);
-  if (event.srcElement[image] === "1") {
-    products[indexes[0]].clickCounter++;
-  } else if (event.srcElement[imageI2] === "2") {
-    products[indexes[1]].clickCounter++;
-  } else if (event.srcElement[imageI3] === "3") {
-    products[indexes[2]].clickCounter++;
-  }
-  if (roundCount >= rounds) {
-    footerElement = document.getElementsByTagName("footer")[0];
-    if (footerElement.firstElementChild) {
-      footerElement.firstElementChild.remove();
+function onClickHandler(event) {
+  const id = event.target.id;
+  roundCount++;
+  products[indexes[id]].clickCount++;
+  doRound();
+  console.log(products);
+  if (roundCount === rounds) {
+    for (let i = 0; i < imageElements.length; i++) {
+      imageElements[i].removeEventListener("click", onClickHandler);
     }
-    footerElement.textContent = "You have finished.";
-    removeListener();
   }
+}
+function start() {
+  for (let i = 0; i < imageElements.length; i++) {
+    console.log("Event listed on the images");
+    imageElements[i].addEventListener("click", onClickHandler);
+  }
+  doRound();
+}
+let resultButton = document.createElement("button");
+resultButton.innerHTML = "Click for Results";
+//below line targets a div on the html page that exists as a placeholder
+let resultPanel = document.getElementById("button-spot");
+resultPanel.appendChild(resultButton);
+resultButton.addEventListener("click", displayResults);
+
+function displayResults() {
+  let resultList = document.getElementById("result-list");
+  for (let i = 0; i < products.length; i++) {
+    let newListItem = document.createElement("li");
+    newListItem.textContent = `${products[i].name} was clicked on ${products[i].clickCount} times, and was shown ${products[i].shownCount} times.`;
+    resultList.appendChild(newListItem);
+    let percentageListItem = document.createElement("li");
+    if (products[i].displayCount === 0) {
+      let math = `ZERO clicks and shown ${products[i].shownCount} times.`;
+    } else {
+      math =
+        Math.round(
+          (
+            products[i]["clickCount"] /
+            products[i]["shownCount"]
+          ).toFixed(2) * 100
+        ) + "%";
+    }
+    percentageListItem.textContent =
+      `${productContainer[i].name} percentage of clicks was` + math;
+    resultList.appendChild(percentageListItem);
+  }
+}
+runMyChart();
+
+function runMyChart() {
+  let charts = document.getElementById("Chart").getContext("2d");
+  let Chart = new Chart(charts, {
+    type: "bar",
+    data: {
+      labels: Product("name"),
+      datasets: [
+        {
+          label: "num of Votes",
+          data: Product("clickCount"),
+          backgroundColor: [
+            "rgba(55, 99, 132, 0.2)",
+            "rgba(154, 162, 235, 0.2)",
+            "rgba(155, 206, 86, 0.2)",
+            "rgba(175, 192, 192, 0.2)",
+            "rgba(103, 102, 255, 0.2)",
+            "rgba(205, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(200, 199, 132, 1)",
+            "rgba(154, 162, 235, 1)",
+            "rgba(55, 206, 86, 1)",
+            "rgba(175, 192, 192, 1)",
+            "rgba(253, 102, 255, 1)",
+            "rgba(155, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  });
 }
